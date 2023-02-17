4f7f37052b45ca44571f76d0d55949cf08b2aa00
Potsawat Panya <125641945+cakezonesee@users.noreply.github.com>
2023-02-17 16:04:07 +0700

    Update Guess_num_game.py

--- Guess_num_game.py
+++ Guess_num_game.py
@@ -1,21 +1,31 @@
+# Import 'random' module to randomize the target number
 import random
-import Dice_random
+
 
 class num_game:
 
+    # 'score' is the number of guess you can have
     score = 4
+    
+    # Randomize target number in the range from 0 - 30
     rand_num = random.randint(0, 30)
     prime_list = []
 
+    
     def __init__(self, guess_num):
         self.guess_num = guess_num
-
+    
+    # Crete function 'guess' so that the code will check if the guess is correct or not
     def guess(self):
+        
+        # If the guess is correct, it will show the target number and your score as in how many life you have left
         if self.guess_num == num_game.rand_num:
             print("CORRECT!!")
             print("Target number is", num_game.rand_num)
             print("Your guess is", self.guess_num)
             print("Your score is", num_game.score)
+            
+        # If the guess is not correct, it decrease the 'score' by one and will show the hint according to how many 'score' you have left
         else:
             num_game.score -= 1
             if num_game.score != 0:
@@ -68,23 +78,27 @@ class num_game:
                         print("This number is a prime number")
                         print("Your current score is", num_game.score)
                         print("_______________________________________________")
+                        
+            # if the 'score' is equal to 0, game will be over
             else:
                 print("_______________________________________________")
                 print("Unlucky! Try again next time")
                 print("The correct answer is", num_game.rand_num)
                 print("_______________________________________________")
 
+    # Crete a function so that the player can continue guessing untill the game is over           
     def play(self):
         if self.guess_num == num_game.rand_num or num_game.score == 0:
             return False
         elif self.guess_num != num_game.rand_num and num_game.score != 0:
             return True
 
-
+# Player input the guess
 guess_num = int(input("Enter your guess: "))
 guess_game = num_game(guess_num)
 guess_game.guess()
 
+# If function 'play' return as 'True' player will be able to keep guessing. If not the program will finish running
 while guess_game.play():
     guess_num = int(input("Enter your guess: "))
     guess_game = num_game(guess_num)
